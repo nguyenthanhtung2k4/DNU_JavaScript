cf03bd707fd785255f9cf70568dd6bea9bdce287
JUSST <hoangsontung2k4@gmail.com>
2024-02-20 10:08:17 +0700

    20/2/04

---
+++ tk_function.js
@@ -0,0 +1,9 @@
+function tesst() {
+     var so=''
+     for (var tung of  arguments) {
+         so +=`${tung} : `
+     }
+      console.log(so);
+}
+tesst('a','b',123,true,'c')
+/* sử dụng  gần như python khác nhau  một  chút  về cách  khai  báo biến  sd (var)
