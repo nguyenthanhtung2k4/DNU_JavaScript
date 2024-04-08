ca034100decfbac02d2f8d4530bf814b8a47088f
JUSST <88431113+nguyenthanhtung2k4@users.noreply.github.com>
2024-04-08 12:55:23 +0700

    wweb upload

---
+++ Classs_DNU/js/index.js
@@ -0,0 +1,8 @@
+
+     <script type="text/javascript" languge="javascript">
+          var a,b,c
+          a=parseFloat(frompt('so A: '))
+          b=parseFloat(frompt('so B: '))
+          c=a+b;
+          document.write(c)
+     </script>
---
+++ tk_Min_Max.js
@@ -0,0 +1,6 @@
+var a =1
+var b =2
+var b =3
+var b =4
+var min=Math.min(a,b,c,d)
+var max=Math.max(a,b,c,d)
